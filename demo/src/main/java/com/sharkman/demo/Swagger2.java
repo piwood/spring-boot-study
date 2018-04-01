ce1e6947040d7d8a391127ba899862e10757b9f3
piwood <526478642@qq.com>
2018-04-01 22:06:35 +0800

    添加swagger依赖

---
+++ demo/src/main/java/com/sharkman/demo/Swagger2.java
@@ -0,0 +1,18 @@
+    /**
+ * 
+ */
+package com.sharkman.demo;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * <p> Description: Swagger2</p>
+ * <p> CreationTime: 2018年4月1日 下午10:04:25
+ * @author piwood
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+}
